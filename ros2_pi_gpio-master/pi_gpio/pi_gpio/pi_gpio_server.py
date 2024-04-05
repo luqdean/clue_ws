9dfed084e9b41865bb1e50bf217268d982a270fe
Allen Windows <allendy.ding@outlook.com>
2024-04-05 19:36:47 +1100

    Recover to be direction

--- ros2_pi_gpio-master/pi_gpio/pi_gpio/pi_gpio_server.py
+++ ros2_pi_gpio-master/pi_gpio/pi_gpio/pi_gpio_server.py
@@ -107,8 +107,8 @@ class GPIOActionServer(Node):
         # Publish the feedback
         goal_handle.publish_feedback(feedback_msg)
 
-        angle = goal_msg  
-        self.pin_dic[self.pin_id].set_pin(angle)
+        direction = goal_msg  
+        self.pin_dic[self.pin_id].set_pin(direction)
         time.sleep(0.1)
         result.value = 3
         
